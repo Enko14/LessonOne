2b3f7b0019aacaada8c73d0049510a89c3b27c9a
Enko14 <enkooo14@gmail.com>
2018-11-21 22:53:21 +0300

    Созданы выводы разных операций

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirst.java
@@ -1,4 +1,17 @@
 package ru.stqa.pft.sanbox;
 
 public class MyFirst {
+  public static void main(String[] args) {
+    System.out.println("HelloWorld");
+    System.out.println(2 + 2);
+    System.out.println(2 * 2);
+    System.out.println(2 / 2);
+    System.out.println(2 - 2);
+    System.out.println(1.0 / 2);
+    System.out.println("2" + "2");
+    System.out.println("2" + 2);
+    System.out.println((2 + 2) * 2);
+    System.out.println("2 + 2 = " + (2 + 2));
+
+  }
 }
