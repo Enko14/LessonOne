411f86f275acd69b20a8b569837045a2ac92682e
Enko14 <enkooo14@gmail.com>
2018-11-21 23:05:23 +0300

    Revert "Созданы выводы разных операций"
    This reverts commit 2b3f7b0019aacaada8c73d0049510a89c3b27c9a.
    
    # Conflicts:
    #	.gitignore

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirst.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirst.java
@@ -1,17 +1,4 @@
 package ru.stqa.pft.sanbox;
 
 public class MyFirst {
-  public static void main(String[] args) {
-    System.out.println("HelloWorld");
-    System.out.println(2 + 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(2 - 2);
-    System.out.println(1.0 / 2);
-    System.out.println("2" + "2");
-    System.out.println("2" + 2);
-    System.out.println((2 + 2) * 2);
-    System.out.println("2 + 2 = " + (2 + 2));
-
-  }
 }
