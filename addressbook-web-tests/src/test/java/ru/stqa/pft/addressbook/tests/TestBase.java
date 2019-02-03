c31218b2b2a44d8797a2a0f8340dbae70cbd3740
Enko14 <enkooo14@gmail.com>
2019-02-03 23:53:29 +0300

    Добавлено описание стенда и вынесены пропертис

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,13 +5,20 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.io.IOException;
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() {
-    app.init();
+    try {
+      app.init();
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+
   }
 
 
