8db7917eb1b0b827cab744fed80730d529e2be53
Enko14 <enkooo14@gmail.com>
2019-02-04 00:17:54 +0300

    Добавлено логгирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/Groups/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/Groups/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.tests.Groups;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -21,6 +23,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXML() throws IOException {
@@ -56,6 +59,7 @@ public class GroupCreationTests extends TestBase {
 
   @Test(dataProvider = "validGroupsFromJSON")
   public void testGroupCreation(GroupData group) {
+
     //GroupData group = new GroupData().withName(name).withHeader(header).withFooter(footer);
     app.goTo().GroupPage();
     Groups before = app.group().all();
@@ -63,6 +67,7 @@ public class GroupCreationTests extends TestBase {
     assertThat(app.group().getGroupCount(), equalTo(before.size() + 1));
     Groups after = app.group().all();
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
   }
 
   @Test
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,14 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.tests.Groups.GroupCreationTests;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class TestBase {
-
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
   protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
@@ -21,10 +27,18 @@ public class TestBase {
 
   }
 
-
   @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
 
+  @BeforeMethod(alwaysRun = true)
+  public void logTestStart(Method m) {
+    logger.info("Start test "+m.getName());
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop test "+m.getName());
+  }
 }
