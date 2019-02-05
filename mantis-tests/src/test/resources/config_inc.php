b9f58ab05e4544499f1fa99d9da7a602d2ca8e12
Enko14 <enkooo14@gmail.com>
2019-02-06 00:44:39 +0300

    Небольшое изменение интерфейса на английский язык

--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -11,3 +11,4 @@ $g_crypto_master_salt     = 'fwh2220+mymWekYRF2sIkudKKd8yYT8hVLizQZ0rhn8=';
 $g_signup_use_captcha=OFF;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
 $g_smtp_host = 'localhost';
+$g_default_language = 'english';
