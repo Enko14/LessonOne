5b7f621002edb6df5dd951f92b4012ffaef9e23a
Enko14 <enkooo14@gmail.com>
2019-02-06 00:30:15 +0300

    Добавлен тест для работы с почтой по telnet

--- mantis-tests/src/test/java/ru/stqa/pft/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/tests/RegistrationTests.java
@@ -1,33 +1,35 @@
 package ru.stqa.pft.tests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.model.MailMessage;
 
+import javax.mail.MessagingException;
 import java.io.IOException;
 import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
-  @BeforeMethod
+  // @BeforeMethod
   public void startMail() {
     app.mail().start();
   }
 
   @Test
-  public void testRegistration() throws IOException {
+  public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
-    String email = String.format("user%s@localhost.localdomain", now);
+    String email = String.format("user%s@localhost", now);
     String user1 = String.format("user%s", now);
     String password = "password";
+    app.james().createUser(user1, password);
     app.registration().start(user1, email);
-    List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    // List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    List<MailMessage> mailMessages = app.james().waitForMail(user1, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user1, password));
+    app.james().deleteUser(user1);
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
@@ -36,7 +38,7 @@ public class RegistrationTests extends TestBase {
     return regex.getText(mailMessage.text);
   }
 
-  @AfterMethod(alwaysRun = true)
+  //@AfterMethod(alwaysRun = true)
   public void stopMail() {
     app.mail().stop();
   }
